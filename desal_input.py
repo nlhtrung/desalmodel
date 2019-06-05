2bad77326e0589adc0566cc9ea23a7a8c709d0e9
nlhtrung <nlhtrung@gmail.com>
2019-06-05 18:15:45 +0200

    update comment

--- desal_input.py
+++ desal_input.py
@@ -20,16 +20,16 @@ membrane = {
     "module diameter": 0.2,     # m
     "spacer thickness": 0.0008, # m
     "module length": 1,         # m
-    "a1": 0.802,
-    "a2": -0.549,
-    "a3": 0,
-    "b1": 0.970,
-    "b2": 0.432,
-    "b3": 1.212,
-    "lw0": 24.478,
-    "ls0": 1.879,
-    "x": 0.000233,
-    "y": 1.735,
+    "a1": 0.802,                # temperatur
+    "a2": -0.549,               # feec concentration
+    "a3": 0,                    # scaling 
+    "b1": 0.970,                # temperature
+    "b2": 0.432,                # feec conc. 
+    "b3": 1.212,                # scaling 
+    "lw0": 24.478,              # solvent permeability
+    "ls0": 1.879,               # solute permeability
+    "x": 0.000233,              # friction drop 
+    "y": 1.735,                 # friction drop
     "membrane type": "BWRO"     # "SWRO": seawater RO or "BWRO": brackish RO or "NF": nanofiltration
 }
 
